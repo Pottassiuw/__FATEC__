8d12c83ce6e9c9ae812f6675c4b61564cf09e638
Pottassiuw <pottassiu@proton.me>
2025-10-17 10:26:47 -0300

    File: Added some class files

---
+++ Python/lista XIV.py
@@ -0,0 +1,22 @@
+#Conceitos de funções (yield e lambda)
+# def f ():
+#     yield 'abacate'
+#     yield 42
+#     yield 'fim'
+
+
+# x = f()
+
+# print(next(x))
+
+# print(next(x))
+
+#variaveis globais
+a = 42
+def f():
+    global a
+    a = 13
+    print(a)
+print(a)
+f()
+print(a)
